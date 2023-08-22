2c3e240584b910934238245bc92ea1169bd8648c
asahitoyotae <asahitoyotae@gmail.com>
2023-08-22 11:24:15 +0800

    new icon

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
-"use client";
-
 import Bars from "./bar-chart-race/page";
 import Navbar from "./components/nav";
 import Options from "./components/options";
 import Footer from "./components/footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="h-full flex flex-col items-center justify-between">
-      <Navbar />
-      <Bars />
-      <Options />
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <link rel="icon" href="/faveicon.png" />
+      </Head>
+      <main className="h-full flex flex-col items-center justify-between">
+        <Navbar />
+        <Bars />
+        <Options />
+        <Footer />
+      </main>
+    </>
   );
 }
